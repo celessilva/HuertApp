a000bd7636169047ef7bad03b3464c878058d16e
lib-paul <libpaul@outlook.com>
2022-11-22 17:39:04 -0300

    Change redirection when creating user
    *Cambiada la redirección al crear un usuario nuevo, si todo salió bien redirige al index.
    
    Co-authored-by: celessilva <96700672+celessilva@users.noreply.github.com>
    Co-authored-by: nlezamapatino <lezamanaivis@gmail.com>
    Co-authored-by: vlopezperea <103610909+vlopezperea@users.noreply.github.com>
    Co-authored-by: Federico Juarez <58090385+XxfedexX@users.noreply.github.com>

--- controladoras/controladorUsuario.py
+++ controladoras/controladorUsuario.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import request,flash,render_template,make_response
+from flask import request,flash,render_template,make_response,redirect,url_for
 from model.forms import UsuarioForm, verificate_username_exist, verificate_username_exist_create
 from model.forms import create_user_database
 from model.consultasPublicacion import get_usuario_by_username
@@ -10,16 +10,18 @@ from werkzeug.security import generate_password_hash
 def create_user():
     title = "Crear Cuenta"
     form = UsuarioForm(request.form)
+    
     if request.method == 'POST':
         nombre = form.nombre.data
-        email = form.username.data
+        email = form.username.data   
         password = generate_password_hash(form.password.data) 
         if verificate_username_exist_create(email):
             flash(f"Otro usuario con email {email} ya existe")
         else:
             create_user_database(nombre,email,password)
             flash(f"Usuario: {nombre} creado")
-    return render_template('create_user.html', title=title, form=form )
+            return redirect(url_for("index"))
+    return render_template("create_user.html", title=title, form=form )
 
 
 
